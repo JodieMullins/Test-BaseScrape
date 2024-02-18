c7e207dd98e4d77264b2569ce5843c7e3e8abc57
Jodie Mullins <jmullins2460@gmail.com>
2024-02-18 17:02:31 -0500

    added tables

--- TSdotcomDatabase.py
+++ TSdotcomDatabase.py
@@ -10,50 +10,106 @@ click = conn.cursor()
 
 
 # function for less future typing
-def est(sql_command):
+def est('''sql_command'''):
     # sql call
-    click.execute(sql_command)
+    click.execute('''sql_command''')
 
 
 # function to execeutemany and commit changes
-def dbc(insertion, access):
+def dbc(insertion, data):
     # sql needs to click around to insert all data points parsed from csv
-    click.executemany(insertion, access)
+    click.executemany(insertion, data)
     # ensure changes are made to the database
     click.commit()
 
 
 
 # ACCESS TABLE
-# Contains Primary Key (Title) + url + availability
+# Contains Primary Key (id) + url + availability
 
 # Fresh start / delete table if present
 est('''DROP TABLE IF EXISTS access''')
 
 # create table in database
 est('''CREATE TABLE access(
-    ID INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     url TEXT,
     availability TEXT)''')
 
-# open file and auto-close when done with it
-with open(r'dotcomdata/bookdata.csv') as accessfile:
+# open file to read and auto-close when done with it
+with open(r'dotcomdata/bookdata.csv', 'r') as af:
 
-    # hold values parsed through
-    access_sheet = csv.DictReader(accessfile)
+# read through the csv
+    access_sheet = csv.DictReader(af)
 
+# hold all instances of iterating through 'url' and 'availbility'
+    data_access = [(i['url'], i['availability']) for i in access_sheet]
 
-    data_access = [(i['url'], i['availability'], i['title']) for i in access_sheet]
-
-
+# put values into the table
 access_insertion = '''INSERT INTO access(
-    id,
-    url,
-    availability,
-    title) VALUES (?, ?, ?, ?)'''
+    url TEXT NOT NULL,
+    availability TEXT NOT NULL) 
+    VALUES (?, ?)'''
 
+# sql, click through inserting all data as detailed into table and commit changes
 dbc(access_insertion, data_access)
 
 
 
 # BOOK TABLE
+# CONTAINS title, product type, category, description, FOREIGN KEY (id)
+
+# Fresh start / delete table if exists
+est('''DROP TABLE IF EXISTS book''')
+
+# create book table for database
+est('''CREATE TABLE book(
+    title TEXT NOT NULL, 
+    product_type, 
+    category, 
+    description, 
+    FOREIGN KEY (id)
+        REFERENCES access (id)
+)''')
+
+with open (r'dotcomdata/bookdata.csv', 'r') as bf:
+
+    book_sheet = csv.DictReader(bf)
+
+# iterating through the csv and holding all instances as identified in i[]
+    data_book = [(i['title'],
+                i['product_type'],
+                i['category'],
+                i['description']) for i in book_sheet]
+
+# insert data taken from csv and input into table for database
+book_insertion = '''INSERT INTO book(
+    title, 
+    product type, 
+    category,
+    description)
+    VALUES (?, ?, ?, ?)'''
+
+# finish book table / commit changes
+dbc(book_insertion, data_book)
+
+
+
+# MONEY TABLE
+# PRICE, TAX, AND PRICE INCL_EXCL TAX
+
+# Fresh Start / Money Table:
+est('''DROP TABLE IF EXISTS money''')
+
+# Establish clean money table
+est('''CREATE TABLE money(
+    price_exl_tax,
+    price_incl_tax,
+    tax,
+    price,
+    FOREIGN KEY (id)
+        REFERENCES access (id)
+)''')
+
+
+
